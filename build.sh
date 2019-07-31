6a5ecc71957153b65ff08d43f6cff7a8cb8f6553
Gabriel FERREIRA <gferreir@c4r24s5.wethinkcode.co.za>
2019-07-31 14:47:16 +0200

    edited stff

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 find . -name "*.java" > sources.txt
 javac --release 7 -sourcepath . @sources.txt
-java -cp ./src za.co.julianwolf.Main scenario.txt
+java -cp ./src com.gabriel.classes.Main scenario.txt
